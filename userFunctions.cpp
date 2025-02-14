92dcb3860338771b88c015a7e402eb224fd7070b
MWPSBCID <nik.zemljic04@gmail.com>
2025-02-14 20:26:02 +0100

    Separated user function into its own header.

---
+++ userFunctions.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include "entryTypes.h"
+#include "globalVariables.h"
+#include <iostream>
+
+
+int readUsers(std::vector<User>& knownUsers) {
+	if (GET_USERNAMES) {
+		std::ifstream usersFile("users.txt");
+		User u;
+		std::string buf;
+		while (true) {
+			usersFile >> u;
+			if (usersFile.fail()) break;
+			knownUsers.push_back(u);
+			getline(usersFile, buf);
+		}
+		usersFile.close();
+	}
+
+
+	return 0;
+}
+
+int saveUsers(std::vector<User>& knownUsers) {
+
+	if (GET_USERNAMES) {
+		std::ofstream usersFile("users.txt");
+		for (User u : knownUsers) {
+			usersFile << u << std::endl;
+		}
+		usersFile.close();
+	}
+
+	return 0;
+}
+
+int printUserDebug(std::vector<User>& knownUsers) {
+
+	if (DEBUG_INFO) {
+		for (auto u : knownUsers) {
+			std::cout << u << ": " << u.entries.size() <<  std::endl;
+			for (auto e : u.entries) {
+
+				std::cout << "Entry: " << "	" << e << std::endl;
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ userFunctions.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include "entryTypes.h"
+
+
+int readUsers(std::vector<User>& knownUsers);
+
+
+
+int saveUsers(std::vector<User>& knownUsers);
+
+
+
+int printUserDebug(std::vector<User>& knownUsers);
